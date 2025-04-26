472d15b041267c0658425a671a4eba628f5716c3
jauntyjocularjay <77408854+jauntyjocularjay@users.noreply.github.com>
2025-04-25 23:57:25 -0700

    added ReactInputs module

--- src/components/Navigator/Navigator.tsx
+++ src/components/Navigator/Navigator.tsx
@@ -1,25 +1,33 @@
-import React from 'react'
 import { location } from '../../Locations.json'
 import Styling from '../../Styling'
 
 interface NavigatorProps {
-    className: string;
+    className: string
 }
+// interface Potion {
+//     price: number
+//     available: number
+// }
 interface Location {
-    alias: string;
+    alias: string
+    // potion: Potion
 }
 
-const Navigator: React.FC<NavigatorProps> = ({className}) => {
-  return (
-    <div className={className}>
-        <span className={Styling.header1}>GPS</span>
-        {
-            location.map((location: Location, index: number) => (
-                <button type='button' key={index} className={Styling.flexButton}>{location.alias}</button>
-            ))
-        }
-    </div>
-  )
+const Navigator: React.FC<NavigatorProps> = ({ className }) => {
+    return (
+        <div className={className}>
+            <span className={Styling.header1}>GPS</span>
+            {location.map((location: Location, index: number) => (
+                <button
+                    type='button'
+                    key={index}
+                    className={Styling.flexButton}
+                >
+                    {location.alias}
+                </button>
+            ))}
+        </div>
+    )
 }
 
 export default Navigator
